24f8aa94dffe46ef3882ee9849c9343e0d3543c9
HoleHorse <tamirlankulzhanov@gmail.com>
2023-06-14 11:02:54 +0600

    item page + payment form

--- CoffeeBase/app/drink/[id].js
+++ CoffeeBase/app/drink/[id].js
@@ -1,9 +1,20 @@
-import { View, Text, ActivityIndicator } from "react-native";
+import {
+  View,
+  Text,
+  ActivityIndicator,
+  TouchableOpacity,
+  Image,
+  TextInput,
+} from "react-native";
 import Drawer from "../../components/Drawer/Drawer";
 import { useLocalSearchParams } from "expo-router";
 import { useEffect, useState } from "react";
 import { Provider, useSelector } from "react-redux";
 import { store } from "../../store/store";
+import { useTranslation } from "react-i18next";
+import styles from "../login/styles";
+import Payment from "react-native-payment";
+import formStyles from "../login/styles";
 
 const DrinkPage = () => {
   return (
@@ -18,6 +29,8 @@ const Drink = () => {
   const { id } = useLocalSearchParams();
   const [item, setItem] = useState({});
   const [loading, setLoading] = useState(true);
+  const { t } = useTranslation();
+  const [visible, setVisible] = useState(false);
 
   useEffect(() => {
     getItem(id);
@@ -47,10 +60,102 @@ const Drink = () => {
   }
   return (
     <Drawer>
-      <View>
-        <Text style={{ color: "#fff" }}>{item.description }</Text>
+      <View style={{ flex: 1 }}>
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+          }}
+        >
+          <Image
+            source={require(`../../assets/img/c7.png`)}
+            style={{ width: 100, height: 200, resizeMode: "contain" }}
+          />
+          <View style={{ display: "flex", flexDirection: "column" }}>
+            <Text style={{ color: "white", fontSize: 22 }}>{item.title}</Text>
+            <Text style={{ color: "white", fontSize: 16 }}>
+              {item.description}
+            </Text>
+            <Text style={{ color: "white", fontSize: 20 }}>${item.price}</Text>
+
+            {visible ? (
+              <TouchableOpacity
+                style={formStyles.btn}
+                onPress={() => {
+                  setVisible(false);
+                }}
+              >
+                <Text style={formStyles.btnText}>{t("cancel")}</Text>
+              </TouchableOpacity>
+            ) : (
+              <TouchableOpacity
+                style={styles.btn}
+                onPress={() => {
+                  setVisible(true);
+                }}
+              >
+                <Text style={styles.btnText}>{t("buy")}</Text>
+              </TouchableOpacity>
+            )}
+          </View>
+        </View>
       </View>
+      {visible && <PaymentForm />}
     </Drawer>
+  );
+};
+
+const PaymentForm = () => {
+  const [cardNumber, setCardNumber] = useState("");
+  const [expiryMonth, setExpiryMonth] = useState("");
+  const [expiryYear, setExpiryYear] = useState("");
+  const [cvc, setCVC] = useState("");
+  const { t } = useTranslation();
+
+  const handlePayment = () => {
+    const paymentData = {
+      number: cardNumber,
+      expMonth: expiryMonth,
+      expYear: expiryYear,
+      cvc: cvc,
+    };
+  };
+
+  return (
+    <View>
+      <TextInput
+        style={formStyles.input}
+        placeholder={t("pcn")}
+        value={cardNumber}
+        onChangeText={setCardNumber}
+      />
+      <View
+        style={{ display: "flex", flexDirection: "row", marginVertical: 5 }}
+      >
+        <TextInput
+          style={[formStyles.input, { width: "50%" }]}
+          placeholder={t("pem")}
+          value={expiryMonth}
+          onChangeText={setExpiryMonth}
+        />
+        <TextInput
+          style={[formStyles.input, { width: "50%" }]}
+          placeholder={t("pey")}
+          value={expiryYear}
+          onChangeText={setExpiryYear}
+        />
+      </View>
+      <TextInput
+        style={formStyles.input}
+        placeholder="CVC"
+        value={cvc}
+        onChangeText={setCVC}
+      />
+      <TouchableOpacity style={formStyles.btn} onPress={handlePayment}>
+        <Text style={formStyles.btnText}>{t("pay")}</Text>
+      </TouchableOpacity>
+    </View>
   );
 };
 
---
+++ CoffeeBase/app/drink/styles.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  btn: {
+    width: "fit-content",
+    backgroundColor: "#2C2F48",
+    paddingLeft: 30,
+    paddingRight: 30,
+    paddingTop: 10,
+    paddingBottom: 10,
+    borderRadius: 10,
+  },
+  btnText: {
+    fontSize: 18,
+    fontFamily: "ChkLightMedium",
+    color: "#fff",
+  },
+});
